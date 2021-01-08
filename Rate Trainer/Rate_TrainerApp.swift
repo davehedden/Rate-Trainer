32d099d5ee80fa9ad2828b93bb225c7d2a7236f6
David Hedden <wavmaker11@gmail.com>
2021-01-08 14:45:48 -0500

    Initial Commit

---
+++ Rate Trainer/Rate_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rate_TrainerApp.swift
+//  Rate Trainer
+//
+//  Created by Dave Hedden on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Rate_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
